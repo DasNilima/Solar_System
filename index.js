19322c88e439ebc628141f7de385fa348c37fd9c
DasNilima <nilima_sust@yahoo.com>
2024-09-13 17:23:19 -0700

    add features & functionality

--- index.js
+++ index.js
@@ -45,3 +45,77 @@ const swiper = new Swiper('.swiper', {
     }
 
 });
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up")
+
+    if (this.scrollY >=250) {
+        scrollUpBtn.classList.remove("-bottom-1/2")
+        scrollUpBtn.classList.add("bottom-4")
+    }
+    else {
+        scrollUpBtn.classList.add("-bottom-1/2")
+        scrollUpBtn.classList.remove("bottom-4")
+    }
+}
+window.addEventListener("scroll", scrollUp)
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+const scrollHeader = () => {
+    const header = document.getElementById("navbar")
+
+    if (this.scrollY >=50) {
+        header.classList.add("border-b", "border-yellow-500")
+    }
+    else {
+        header.classList.remove("border-b", "border-yellow-500")
+    }
+}
+window.addEventListener("scroll", scrollHeader)
+
+
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+const activeLink = () => {
+
+    const sections = document.querySelectorAll('section')
+    const navLinks = document.querySelectorAll('.nav-link')
+
+    let current = "home"
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if (this.scrollY >= sectionTop - 60) {
+            current = section.getAttribute("id")
+        }
+    })
+    navLinks.forEach(item => {
+        item.classList.remove('active')
+        if (item.href.includes(current)) {
+            item.classList.add("active")
+        }
+    })
+}
+
+window.addEventListener("scroll",activeLink)
+
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 300,
+    reset: true
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__image`, { delay: 500, scale: 0.5})
+sr.reveal(`.overview__data`, { interval: 100 })
+sr.reveal(`.moons__card`, { interval: 100 })
+sr.reveal(`.celestial__card`, { interval: 100 })
+sr.reveal(`.planets__title, .moons__title, .celestial__title`)
+sr.reveal(`.astronaut__image-1`, { origin: 'right' })
+sr.reveal(`.astronaut__image-2`, { origin:'left'})
